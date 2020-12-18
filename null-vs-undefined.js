d27edf9d92b93f801c94645c981dca9dade92b1a
JoyMazumdar379 <joymazumdar379@gmail.com>
2020-12-18 16:43:03 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+// undefined Value: let name;
+
+function add(num1, num2) {
+console.log(num1 + num2);
+return;
+}
+
+function add(num1, num2) {
+   console.log(num1, num2);
+}
+add(10);
+
+const name = {name : "smart dude", phone : 456987};
+console.log(name.gf);
+
+let fun = undefined;
+console.log(fun);
+
+// Null Value: pass or set kore dite hobe
